bcfa5864c500625297fff061ce31c18540bf697e
christhedn <christian.deitert@gmail.com>
2020-05-22 05:17:44 +0200

    deploy

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -21,7 +21,7 @@ class SignIn extends React.Component {
 
     onSubmitSignIn = () => {
         console.log(this.state);
-        fetch('https://blooming-beach-94945.herokuapp.com//signin', {
+        fetch('https://blooming-beach-94945.herokuapp.com/signin', {
             method: 'post', 
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
